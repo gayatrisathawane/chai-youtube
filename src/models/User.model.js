898b80266505e996ad437198d45508daf9a67f71
gayatrisathawane <gayatripsathawane@gmail.com>
2023-11-24 18:36:51 +0530

    added jwt tokens

--- src/models/User.model.js
+++ src/models/User.model.js
@@ -1,54 +1,83 @@
-import {model,Schema} from 'mongoose'
+import { model, Schema } from 'mongoose'
+impo
 
 const userSchema = new Schema({
 
-    username:{
-        type:String,
-        required:true,
-        unique:true,
-        lowercase:true,
-        trim:true,
-        index:true
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        index: true
     },
-    email:{
-        type:String,
-        required:true,
-        lowercase:true,
-        unique:true,
-        trim:true
+    email: {
+        type: String,
+        required: true,
+        lowercase: true,
+        unique: true,
+        trim: true
     },
-    fullname:{
-        type:String,
-        required:true,
-        trim:true,
-        index:true
+    fullname: {
+        type: String,
+        required: true,
+        trim: true,
+        index: true
     },
-    avatar:{
-        type:String,  //cloudinary
-        required:true,
+    avatar: {
+        type: String,  //cloudinary
+        required: true,
 
     },
-    coverImage:{
-        type:String,  //cloudinary
+    coverImage: {
+        type: String,  //cloudinary
     },
-    watchHistory:[
+    watchHistory: [
         {
-            type:Schema.Types.ObjectId,
-            ref:"Video"
+            type: Schema.Types.ObjectId,
+            ref: "Video"
         }
     ],
-    password:{
-        type:String,
-        required:[true,"password is required"]
+    password: {
+        type: String,
+        required: [true, "password is required"]
     },
-    refeshToken:{
-        
+    refeshToken: {
+
     }
 
+}, { timestamps: true })
 
 
+userSchema.methods.generateAccessToken = function () {
+    return Jwt.sign(
+        {
 
-},{timestamps:true})
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullname: this.fullname
+
+        },
+        process.env.ACCESS_TOKEN_SECRET, {
+        expireIn: process.env.ACCESS_TOKEN_EXPIRY
+    }
+    )
+}
+userSchema.methods.generateRefreshToken = function () {
+
+    return Jwt.sign(
+        {
+
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expireIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+
+}
 
 
-export const  User = model('User',userSchema)
+export const User = model('User', userSchema)
